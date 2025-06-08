3b812700d3cf2635d13a3566f2d149da2aeb5343
Aaro Haikonen <aaro.haikonen99@gmail.com>
2025-06-08 12:33:16 +0300

    message queue can now send images in json

--- src/messageq.py
+++ src/messageq.py
@@ -12,35 +12,22 @@ class MessageQueue():
         self.channel.queue_declare(queue=queue_name)
         print(f"{queue_name} declared")
     
-    def get_msg(self, queue_name=""):
-        return self.channel.basic_get(queue=self.queue, auto_ack=True)
-    
-    def add_msg(self, body, queue_name="") -> bool:
-        print(f"{self.queue} sending message")
-        if type(body) == str:
-            # String message
-            self.channel.basic_publish(exchange="", routing_key=self.queue, body=body)
-            return True
-        if type(body) == np.ndarray:
-            # JPG message
-            encoded = cv2.imencode(".jpg", body)[1].tobytes()
-            self.channel.basic_publish(exchange="", routing_key=self.queue, body=encoded)
-            return True
+    def get_msg(self):
+        method, properties, body = self.channel.basic_get(self.queuem, auto_ack=True)
+        body = json.loads(body)
         if type(body) == dict:
-            body = json.dumps(body)
-            self.channel.basic_publish( exchange="", routing_key=self.queue, body=body)
-            return True
-        return False
-
-    def add_queue(self, queue_name) -> bool:
-        if queue_name == self.queue:
-            return False
-        if type(self.channel) != dict:
-            self.channel = {self.queue : self.channel}
-        elif queue_name in self.channel:
-            return False
-        self.channel[queue_name] = self.connection.channel()
-        self.channel[queue_name].queue_declare(queue=queue_name)
+            if "img" in body:
+                array = np.array([np.uint8(x) for x in body["img"]])
+                body["img"] = cv2.imdecode(array, cv2.IMREAD_COLOR)
+        return method, properties, body
+    
+    def add_msg(self, body) -> bool:
+        if type(body) == dict:
+            if "img" in body:
+                body["img"] = [int(x) for x in body["img"]]
+        pkg = json.dumps(body)
+        return self.channel.basic_publish(exchange="", routing_key=self.queue, body=pkg)
+        
 
     def get_blocking_msg(self, callback):
         self.channel.basic_consume(queue=self.queue, on_message_callback=callback, auto_ack=True)
